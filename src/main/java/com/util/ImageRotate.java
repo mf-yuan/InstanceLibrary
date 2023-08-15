75369c2f4114939cf4c22a0d145a7e51d4a00c45
mfyuan <1535659075@qq.com>
2023-08-15 15:07:01 +0800

    纠正图片工具类

--- src/main/java/com/util/ImageRotate.java
+++ src/main/java/com/util/ImageRotate.java
@@ -5,7 +5,6 @@ import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 import com.drew.metadata.exif.ExifIFD0Directory;
-import org.apache.poi.ss.formula.functions.T;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -13,9 +12,9 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.UUID;
 
 public class ImageRotate {
+
 
     /**
      * 纠正图片旋转
@@ -27,9 +26,8 @@ public class ImageRotate {
         try {
             // 原始图片
             File srcFile = new File(srcImgPath);
-
             // 获取偏转角度
-            AngelMirror rotateAngle = getRotateAngle(srcFile);
+            RectifyDirection rotateAngle = getRectifyDirection(srcFile);
             if(rotateAngle == null){
                 return;
             }
@@ -38,17 +36,18 @@ public class ImageRotate {
             // 原始图片缓存
             BufferedImage srcImg = ImageIO.read(srcFile);
 
-            // 宽高互换
-            // 原始宽度
-            int imgWidth = srcImg.getHeight();
-            // 原始高度
-            int imgHeight = srcImg.getWidth();
 
-            // if (rotateAngle.angel != 180) {
-            //     int temp = imgWidth;
-            //     imgWidth = imgHeight;
-            //     imgHeight = temp;
-            // }
+            // 原始宽度
+            int imgWidth = srcImg.getWidth();
+            // 原始高度
+            int imgHeight = srcImg.getHeight();
+
+            // 宽高互换
+            if (rotateAngle.angel != 180) {
+                int temp = imgWidth;
+                imgWidth = imgHeight;
+                imgHeight = temp;
+            }
 
             // 中心点位置
             double centerWidth = ((double) imgWidth) / 2;
@@ -61,24 +60,26 @@ public class ImageRotate {
             Graphics2D g = targetImg.createGraphics();
             // 向左旋转
             g.rotate(Math.toRadians(rotateAngle.angel), centerWidth, centerHeight);
-            g.drawImage(srcImg, (imgWidth - srcImg.getWidth()) / 2, (imgHeight - srcImg.getHeight()) / 2, null);
-            g.rotate(Math.toRadians(-rotateAngle.angel), centerWidth, centerHeight);
-            g.dispose();
+            System.out.println(srcImg.getWidth());
+            System.out.println(srcImg.getHeight());
 
+            int x = (imgWidth - srcImg.getWidth()) / 2;
+            int y = (imgHeight - srcImg.getHeight()) / 2;
+            g.drawImage(srcImg, x, y, null);
+            g.dispose();
             if (rotateAngle.isMirror){
-                g = targetImg.createGraphics();
+                BufferedImage tempImage = new BufferedImage(targetImg.getWidth(), targetImg.getHeight(), BufferedImage.TYPE_INT_RGB);
+                g = tempImage.createGraphics();
                 // 使用 AffineTransform 进行水平翻转
                 AffineTransform transform = new AffineTransform();
-                System.out.println(imgWidth);
-                System.out.println(imgHeight);
-                System.out.println(centerWidth);
-                System.out.println(centerHeight);
-                transform.translate(targetImg.getWidth(), 0);
+                transform.translate(targetImg.getWidth() , 0);
                 transform.scale(-1, 1);
-                g.drawImage(targetImg, transform, null);
-                g.dispose();
-            }
+                g.setTransform(transform);
 
+                g.drawImage(targetImg,0, 0, null);
+                g.dispose();
+                targetImg = tempImage;
+            }
 
             // 输出图片
             fos = new FileOutputStream(new File(srcFile.getAbsolutePath() + "." + srcImgPath.substring(srcImgPath.lastIndexOf(".") + 1)));
@@ -97,51 +98,65 @@ public class ImageRotate {
         }
     }
 
-    public static AngelMirror getRotateAngle(File file) throws Exception {
+    /**
+     * 获取纠正方向
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    public static RectifyDirection getRectifyDirection(File file) throws Exception {
+        // 1.获取图片的元数据
         Metadata metadata = ImageMetadataReader.readMetadata(file);
+        // 2.图片元数据处理那种方向
         int orientation = 0;
 
         for (Directory directory : metadata.getDirectories()) {
-            System.out.println(directory);
-            if (directory.getString(ExifIFD0Directory.TAG_ORIENTATION) != null) {
+            // 3.只需要TAG_ORIENTATION的数据即可。
+            if(directory.containsTag(ExifIFD0Directory.TAG_ORIENTATION)){
+                for (Tag tag : directory.getTags()) {
+                    System.out.println(tag);
+                }
                 orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
             }
         }
-        System.out.println(orientation);
+        // 4.根据不同的orientation创建不同的处理方式
         switch (orientation){
             case 2:
-                return new AngelMirror(0,true);
+                return new RectifyDirection(0,true);
             case 3:
-                return new AngelMirror(180,false);
+                return new RectifyDirection(180,false);
             case 4:
-                return new AngelMirror(180, true);
+                return new RectifyDirection(180, true);
             case 5:
-                return new AngelMirror(90,true);
+                return new RectifyDirection(90,true);
             case 6:
-                return new AngelMirror(90,false);
+                return new RectifyDirection(90,false);
             case 7:
-                return new AngelMirror(270,true);
+                return new RectifyDirection(270,true);
             case 8:
-                return new AngelMirror(270,false);
+                return new RectifyDirection(270,false);
             default:
                 return null;
         }
     }
 
-    static class AngelMirror{
+    static class RectifyDirection{
+        /**
+         * 角度
+         */
         public int angel;
+        /**
+         * 是否镜像
+         */
         public boolean isMirror;
 
-        public AngelMirror(int angel, boolean isMirror) {
+        public RectifyDirection(int angel, boolean isMirror) {
             this.angel = angel;
             this.isMirror = isMirror;
         }
     }
 
-
-
-
     public static void main(String[] args) {
-        ImageRotate.correctImg("C:\\Users\\EDY\\Desktop\\微信图片_20230814102842.jpg");
+        ImageRotate.correctImg("C:\\Users\\EDY\\Desktop\\微信图片_20230814120400.jpg");
     }
 }
