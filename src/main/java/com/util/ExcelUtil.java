b71408ff46c81ca6ed2d8a5944d2f039bf41679a
mfyuan <1535659075@qq.com>
2024-05-16 12:13:15 +0800

    解决SSL问题

--- src/main/java/com/util/ExcelUtil.java
+++ src/main/java/com/util/ExcelUtil.java
@@ -10,8 +10,12 @@ import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.platform.commons.util.StringUtils;
 
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.*;
 import java.util.function.Consumer;
 
@@ -227,7 +231,13 @@ public class ExcelUtil {
                 inputStream = new FileInputStream(filePath);
             } else {
                 // 如果是网络文件 我们用ByteArrayOutputStream 接收然后写入到bytes中即可
-                inputStream = new URL(filePath).openStream();
+
+                // SSL
+                if(filePath.startsWith("https")){
+                    enableSSL();
+                }
+                HttpsURLConnection connection = (HttpsURLConnection) new URL(filePath).openConnection();
+                inputStream = connection.getInputStream();
             }
             byteArrayOutputStream = new ByteArrayOutputStream();
             // 创建存放文件内容的数组
@@ -407,6 +417,38 @@ public class ExcelUtil {
             }
         }
         return null;
+    }
+
+    private static final TrustManager TRUST_MANAGER = new X509TrustManager() {
+
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[] {};
+        }
+
+        public void checkClientTrusted(X509Certificate[] cert, String oauthType)
+                throws java.security.cert.CertificateException {
+        }
+
+        public void checkServerTrusted(X509Certificate[] cert, String oauthType)
+                throws java.security.cert.CertificateException {
+        }
+    };
+
+    private static  final HostnameVerifier ALL_HOSTNAME_VERIFIER = (hostname, session) -> true;
+
+
+    private static void enableSSL() {
+        try {
+            // 安装所有信任的trust manager
+            SSLContext sslcontext = SSLContext.getInstance("TLS");
+            sslcontext.init(null, new TrustManager[] { TRUST_MANAGER }, null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslcontext.getSocketFactory());
+            // 创建一个 HostnameVerifier 始终返回 true
+            // 安装所有信任的 hostname verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(ALL_HOSTNAME_VERIFIER);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
     }
 
 
