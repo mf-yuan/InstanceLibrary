0b87c05fde664835d07ae3ae61d5980e7783fb89
yuanmengfan <1535659075@qq.com>
2024-06-17 00:46:52 +0800

    优化time api

--- src/main/java/com/util/TemporalAccessorUtils.java
+++ src/main/java/com/util/TemporalAccessorUtils.java
@@ -15,9 +15,10 @@ public class TemporalAccessorUtils {
 
     /**
      * // {@link TemporalAccessor}转换为 {@link Instant}对象
-     * @title toInstant
+     *
      * @param temporalAccessor
      * @return java.time.Instant
+     * @title toInstant
      * @author yuanmengfan
      * @date 2022/3/27 5:22 下午
      */
@@ -51,149 +52,117 @@ public class TemporalAccessorUtils {
 
     /**
      * // {@link TemporalAccessor} 转 {@link ZonedDateTime}
-     * @title toZoneDateTime
+     *
      * @param temporalAccessor 时间对象
      * @return java.time.ZonedDateTime
+     * @title toZoneDateTime
      * @author yuanmengfan
      * @date 2022/3/26 11:48 下午
      */
-    public static ZonedDateTime toZonedDateTime(TemporalAccessor temporalAccessor,ZoneId zoneId){
-        if (temporalAccessor == null) {
+    public static ZonedDateTime toZonedDateTime(TemporalAccessor temporalAccessor, ZoneId zoneId) {
+        Instant instant = toInstant(temporalAccessor);
+        if (instant == null) {
             return null;
         }
-        ZonedDateTime result;
-        if (temporalAccessor instanceof Instant) {
-            result = ZonedDateTime.ofInstant((Instant) temporalAccessor, zoneId);
-        } else if (temporalAccessor instanceof ZonedDateTime) {
-            result =  ((ZonedDateTime) temporalAccessor).withZoneSameInstant(zoneId);
-        } else if (temporalAccessor instanceof OffsetDateTime) {
-            result = ((OffsetDateTime) temporalAccessor).toZonedDateTime();
-        } else if (temporalAccessor instanceof LocalDateTime) {
-            result = ((LocalDateTime) temporalAccessor).atZone(zoneId);
-        } else if (temporalAccessor instanceof LocalDate) {
-            // 返回为当前日期的开始的时间也就是 当天的00:00:00.000
-            result = ((LocalDate) temporalAccessor).atStartOfDay().atZone(zoneId);
-        } else if (temporalAccessor instanceof LocalTime) {
-            // 返回为纪元开始的时间 1970-1-1 LocalTime
-            result = ((LocalTime) temporalAccessor).atDate(getEpochDate()).atZone(zoneId);
-        } else if (temporalAccessor instanceof OffsetTime) {
-            // 返回为当前日期的开始的时间也就是 当天的00:00:00.000
-            result = ((OffsetTime) temporalAccessor).atDate(getEpochDate()).atZoneSameInstant(zoneId);
-        } else {
-            result = ZonedDateTime.from(temporalAccessor);
-        }
-        return result;
+        return ZonedDateTime.ofInstant(instant, zoneId);
     }
 
     /**
      * // 默认为本地的时区 {@link TemporalAccessor} 转 {@link ZonedDateTime}
-     * @title toZonedDateTime
+     *
      * @param temporalAccessor
      * @return java.time.ZonedDateTime
+     * @title toZonedDateTime
      * @author yuanmengfan
      * @date 2022/3/27 5:44 下午
      */
-    public static ZonedDateTime toZonedDateTime(TemporalAccessor temporalAccessor){
-        return toZonedDateTime(temporalAccessor,ZoneId.systemDefault());
+    public static ZonedDateTime toZonedDateTime(TemporalAccessor temporalAccessor) {
+        return toZonedDateTime(temporalAccessor, ZoneId.systemDefault());
     }
 
     /**
      * // {@link TemporalAccessor} 转 {@link LocalDateTime}
-     * @title toZoneDateTime
+     *
      * @param temporalAccessor 时间对象
      * @return java.time.ZonedDateTime
+     * @title toZoneDateTime
      * @author yuanmengfan
      * @date 2022/3/26 11:48 下午
      */
-    public static LocalDateTime toLocalDateTime(TemporalAccessor temporalAccessor){
-        if (temporalAccessor == null) {
-            return null;
-        }
-        LocalDateTime result;
-        if (temporalAccessor instanceof Instant) {
-            result = LocalDateTime.ofInstant((Instant) temporalAccessor,ZoneId.systemDefault());
-        } else if (temporalAccessor instanceof ZonedDateTime) {
-            result =  ((ZonedDateTime) temporalAccessor).toLocalDateTime();
-        } else if (temporalAccessor instanceof OffsetDateTime) {
-            result = ((OffsetDateTime) temporalAccessor).toLocalDateTime();
-        } else if (temporalAccessor instanceof LocalDateTime) {
-            result = (LocalDateTime) temporalAccessor;
-        } else if (temporalAccessor instanceof LocalDate) {
-            // 返回为当前日期的开始的时间也就是 当天的00:00:00.000
-            result = ((LocalDate) temporalAccessor).atStartOfDay();
-        } else if (temporalAccessor instanceof LocalTime) {
-            // 返回为纪元开始的时间 1970-1-1 LocalTime
-            result = ((LocalTime) temporalAccessor).atDate(getEpochDate());
-        } else if (temporalAccessor instanceof OffsetTime) {
-            // 返回为当前日期的开始的时间也就是 当天的00:00:00.000
-            result = ((OffsetTime) temporalAccessor).atDate(getEpochDate()).toLocalDateTime();
-        } else {
-            result = LocalDateTime.from(temporalAccessor);
-        }
-        return result;
+    public static LocalDateTime toLocalDateTime(TemporalAccessor temporalAccessor) {
+        return LocalDateTime.ofInstant(toInstant(temporalAccessor), ZoneId.systemDefault());
     }
 
     /**
      * // 时间对象转换为 {@link java.util.Date}
-     * @title toDate
+     *
      * @param temporalAccessor
      * @return java.util.Date
+     * @title toDate
      * @author yuanmengfan
      * @date 2022/3/26 11:45 下午
      */
-    public static Date toDate(TemporalAccessor temporalAccessor){
+    public static Date toDate(TemporalAccessor temporalAccessor) {
         return new Date(toInstant(temporalAccessor).toEpochMilli());
     }
 
     /**
      * // {@link java.util.Date} 转 ${@link LocalDateTime}
-     * @title toLocalDateTime
+     *
      * @param date
      * @return java.time.LocalDateTime
+     * @title toLocalDateTime
      * @author yuanmengfan
      * @date 2022/3/27 12:03 上午
      */
-    public static LocalDateTime toLocalDateTime(Date date){
-        return LocalDateTime.ofInstant(date.toInstant(),ZoneId.systemDefault());
+    public static LocalDateTime toLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
+
     /**
      * // {@link java.sql.Date} 转 ${@link LocalDateTime}
-     * @title toLocalDateTime
+     *
      * @param date
      * @return java.time.LocalDateTime
+     * @title toLocalDateTime
      * @author yuanmengfan
      * @date 2022/3/27 12:03 上午
      */
-    public static LocalDateTime toLocalDateTime(java.sql.Date date){
-        return LocalDateTime.ofInstant(date.toInstant(),ZoneId.systemDefault());
+    public static LocalDateTime toLocalDateTime(java.sql.Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
+
     /**
      * // {@link Calendar} 转 ${@link LocalDateTime}
-     * @title toLocalDateTime
+     *
      * @param date
      * @return java.time.LocalDateTime
+     * @title toLocalDateTime
      * @author yuanmengfan
      * @date 2022/3/27 12:03 上午
      */
-    public static LocalDateTime toLocalDateTime(Calendar date){
-        return LocalDateTime.ofInstant(date.toInstant(),ZoneId.systemDefault());
+    public static LocalDateTime toLocalDateTime(Calendar date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
+
     /**
      * // {@link Timestamp} 转 ${@link LocalDateTime}
-     * @title toLocalDateTime
+     *
      * @param date
      * @return java.time.LocalDateTime
+     * @title toLocalDateTime
      * @author yuanmengfan
      * @date 2022/3/27 12:03 上午
      */
-    public static LocalDateTime toLocalDateTime(Timestamp date){
-        return LocalDateTime.ofInstant(date.toInstant(),ZoneId.systemDefault());
+    public static LocalDateTime toLocalDateTime(Timestamp date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
     /**
      * // 获取纪元开始的日期
-     * @title getEpochDate
+     *
      * @return java.time.LocalDate
+     * @title getEpochDate
      * @author yuanmengfan
      * @date 2022/3/27 5:23 下午
      */
@@ -203,8 +172,9 @@ public class TemporalAccessorUtils {
 
     /**
      * // 获取纪元开始的时间
-     * @title getEpochDateTime
+     *
      * @return java.time.LocalDateTime
+     * @title getEpochDateTime
      * @author yuanmengfan
      * @date 2022/3/27 5:23 下午
      */
