76a3ec42f576e0aeac30784d4909e10641025b67
mfyuan <mf.yuan@qq.com>
2025-11-09 23:04:47 +0800

    Update 修改包名为 com.mfyuan

---
+++ src/main/java/com/mfyuan/db/build/sql/ContextBuilder.java
@@ -0,0 +1,14 @@
+package com.mfyuan.db.build.sql;
+
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * @author yuanmengfan
+ * @date 2022/7/23 16:07
+ * @description 生成建表语句中()中的内容
+ */
+public interface ContextBuilder {
+    String context(List<Field> field);
+}
---
+++ src/main/java/com/mfyuan/util/DesUtils.java
@@ -0,0 +1,10 @@
+package com.mfyuan.util;
+
+
+/**
+ * @author yuanmengfan
+ * @date 2021/11/4 2:39 下午
+ * @description
+ */
+public class DesUtils {
+}
